1989eb3ee6f28ad9e0a301fac666127566b64843
Andrea <corro.andrea.1995@gmail.com>
2021-01-28 16:11:33 +0100

    prime funzioni

--- vue/script.js
+++ vue/script.js
@@ -2,17 +2,24 @@ var mui = new Vue ({
   el: '#lista',
   data: {
     toDo: [
-      "spesa",
-      "ginnastica",
-      "esercizi",
-      "chiamare università"
+      'spesa',
+      'ginnastica',
+      'esercizi',
+      'chiamare università'
     ],
     nuovoToDo: '',
     toDoDone: []
   },
   methods: {
-    cancella: function(index){
-      this.toDo.splice(index,1);
+    cancella(index){
+      this.toDoDone.push(this.toDo[index]);
+      // console.log(this.toDoz);
+      // this.toDo.splice(index,1);
+      console.log(this.toDo[index]);
+
+    },
+    aggiungi: function(){
+      this.toDo.push(this.nuovoToDo);
 
     }
   }
